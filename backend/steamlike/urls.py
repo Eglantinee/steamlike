84aa97cedb805cef3dd9db8b8bed09acdc9beef9
Ivan Shkardybarda <ivanshkardybarda@gmail.com>
2021-01-21 22:01:17 +0200

    just make imports looks loke normal one for urls.py cause Root dir wasnt set proper

--- backend/steamlike/urls.py
+++ backend/steamlike/urls.py
@@ -13,16 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import sys
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from django.conf import settings
 from django.conf.urls.static import static
-
-sys.path.append(".")
-sys.path.append("..")
-from backend.books import views
+from books import views
 
 
 router = routers.DefaultRouter()
@@ -31,6 +27,7 @@ router.register(r'book', views.BookViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
 ]
 
 if settings.DEBUG:
