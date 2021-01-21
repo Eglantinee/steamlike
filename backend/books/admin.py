583b33e258f487c850e625c328b1607ab5601aa6
Ivan Shkardybarda <ivanshkardybarda@gmail.com>
2021-01-21 19:16:00 +0200

    Update backend -- add support for admin panel, and register all tables of DB into it

--- backend/books/admin.py
+++ backend/books/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Publisher, Series, Author, Genres, UserGroup, Book, User, BookHasUser, Comment
 
-# Register your models here.
+
+admin.site.register(Publisher)
+admin.site.register(Series)
+admin.site.register(Author)
+admin.site.register(Genres)
+admin.site.register(UserGroup)
+admin.site.register(Book)
+admin.site.register(User)
+admin.site.register(BookHasUser)
+admin.site.register(Comment)
--- backend/steamlike/urls.py
+++ backend/steamlike/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from books import views
+from backend.books import views
 
 router = routers.DefaultRouter()
 router.register(r'book', views.BookViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
