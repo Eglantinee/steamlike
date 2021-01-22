b5c3105b4a3bd15f427233cc98324036bbde819d
Ivan Shkardybarda <ivanshkardybarda@gmail.com>
2021-01-22 17:59:52 +0200

    update for frontend but i think this folder will be not used in future

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <!--This is part where we store html code for page. It is static at all-->
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+// --This is part of   JS   logic. -->
+export default {
+  name: "Login",
+  data() {
+    return {
+      msg: 'Hello!',
+    };
+  },
+}
+</script>
+
+<style scoped>
+/*here we store our styles*/
+</style>
+
+
+<!--Then we gonna add this page into /router/index.js
+  In main APP.vue it is connected into <router view/> -->
