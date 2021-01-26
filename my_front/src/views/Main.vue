a22806766da04b1af2d99047cc09d41c05ef814d
Ivan Shkardybarda <ivanshkardybarda@gmail.com>
2021-01-26 21:17:08 +0200

    Update Frontend:     * update Main page to show filters and make request with filter parameters

--- my_front/src/views/Main.vue
+++ my_front/src/views/Main.vue
@@ -2,15 +2,17 @@
   <div class="container-fluid editContainer">
     <div class="row row-mov">
       <div class="col-2 sidebar">
-        <ul>
-          <template v-for="genre in genres">
-            <li><a>{{ genre.genre_name }}</a></li>
-          </template>
-          <!--          <li><a>First</a></li>-->
-          <!--          <li><a>First</a></li>-->
-          <!--          <li><a>First</a></li>-->
-          <!--          <li><a>First</a></li>-->
-        </ul>
+        <template v-for="genre in genres">
+          <div>
+            <!--suppress HtmlFormInputWithoutLabel -->
+            <input type="checkbox" :id=genre.genre_name :value=genre.genre_name v-model="filter">
+            <label :for=genre.genre_name style="padding-left: 10px">{{ genre.genre_name }}</label>
+          </div>
+        </template>
+        <input type="button" value="Submit-button" v-on:click="filterBook">
+        <div>
+          <p>{{ filter }}</p>
+        </div>
       </div>
       <div class="col-10 list-view">
         <div class="row">
@@ -29,11 +31,13 @@
 
 <script>
 import axios from "axios";
+import qs from "qs"
 
 export default {
   name: "Main",
   data() {
     return {
+      filter: [],
       books: null,
       genres: null
     }
@@ -49,6 +53,20 @@ export default {
           this.genres = response.data
         }
     )
+  },
+  methods: {
+    filterBook: function () {
+      axios.get('http://localhost:8000/book/', {
+        params: {filter: this.filter}, paramsSerializer: params => {
+          return qs.stringify(params, {arrayFormat: "comma"})
+        }
+      }).then(response => {
+        this.books = response.data;
+      })
+    },
+    log(checkboxEvent) {
+      console.log(checkboxEvent)
+    }
   }
 }
 
