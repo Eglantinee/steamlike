5dae069a05c27015671e8d7cecc208a544963178
Ivan Shkardybarda <ivanshkardybarda@gmail.com>
2021-01-29 00:53:10 +0200

    Update Backend:     * add some simple tests

--- backend/books/tests.py
+++ backend/books/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from rest_framework import status
+import requests
 
-# Create your tests here.
+
+class URLTests(TestCase):
+    def test_homepage(self):
+        response = requests.get(url="http://localhost:8000/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_book_url(self):
+        r = requests.get("http://localhost:8000/book/")
+        self.assertEqual(r.status_code, status.HTTP_200_OK)
+
+    def test_login_url(self):
+        r = requests.get("http://localhost:8000/login/")
+        self.assertEqual(r.status_code, status.HTTP_200_OK)
+
+    def test_genre_url(self):
+        r = requests.get("http://localhost:8000/genre/")
+        self.assertEqual(r.status_code, status.HTTP_200_OK)
